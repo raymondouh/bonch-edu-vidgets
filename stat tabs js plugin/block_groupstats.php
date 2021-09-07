a9e3692e9d231ef6efc449f0f6f8076692bcda9d
Andrei Serbin <serbinandr@gmail.com>
2021-09-07 17:03:54 +0300

    promblem studens render

--- stat tabs js plugin/block_groupstats.php
+++ stat tabs js plugin/block_groupstats.php
@@ -1,26 +1,33 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-class block_groupstats extends block_base {
 
-    function hide_header() {
+class block_groupstats extends block_base
+{
+
+    function hide_header()
+    {
         return true;
     }
 
-    function init() {
-       $this->title = get_string('pluginname', 'block_groupstats');
+    function init()
+    {
+        $this->title = get_string('pluginname', 'block_groupstats');
     }
 
-    function get_content() {
+    function get_content()
+    {
+        global $DB,  $CFG, $OUTPUT;
         if ($this->content !== NULL) {
-        return $this->content;
+            return $this->content;
         }
         $this->content = new stdClass;
-        $renderable = new \block_groupstats\output\main();
+        $template = new \block_groupstats\output\main();
         $renderer = $this->page->get_renderer('block_groupstats');
+        $this->content->text = $renderer->render($template);
+        $this->page->requires->js_call_amd('block_groupstats/groupstats', 'init');
+        $this->page->requires->css('/blocks/groupstats/styles.css');
 
-        $this->content->text = $renderer->render($renderable);
-        $this->page->requires->js_call_amd('block_groupstats/groupstats','init');
-        $this->page->requires->css( '/blocks/groupstats/styles.css');
+
         return $this->content;
     }
 }
--- stat tabs js plugin/classes/output/main.php
+++ stat tabs js plugin/classes/output/main.php
@@ -5,13 +5,40 @@ defined('MOODLE_INTERNAL') || die();
 use renderable;
 use renderer_base;
 use templatable;
+use user_picture;
 
 class main implements renderable, templatable {
-    public function export_for_template(renderer_base $output) {
 
-        $imageurl = $output->image_url('student', 'block_groupstats')->out();
+    public function export_for_template(renderer_base $output) {
+        $problemstudents = $this->get_problem_students();
         return [
-            'imgurl' => $imageurl,
+            'problemstudents' => $problemstudents
         ];
+    }
+
+    private function get_problem_students(){
+        global $CFG, $PAGE;
+        $course = get_course("3");
+        $users = groups_get_members('1');
+        $problemstudents = array();
+
+        foreach ($users as $user) {
+            $user_picture=new user_picture($user);
+            $src=$user_picture->get_url($PAGE);
+
+            $userfullname = $user->firstname . ' ' . $user->lastname;
+            $userprofile = $CFG->wwwroot.'/user/view.php?id='.$user->id.'&amp;course='. $course->id;
+
+            //if no userpicture you can get this
+            //$imageurl = $output->image_url('student', 'block_groupstats')->out();
+
+            $problemstudents[] = array(
+
+                'fullname'=> $userfullname,
+                'picturesrc' => $src,
+                'userprofile' => $userprofile
+            );
+        }
+        return $problemstudents;
     }
 }
